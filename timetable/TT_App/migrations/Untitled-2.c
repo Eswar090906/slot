16ff5354eec1217cb400912c1da1dd9942cb0594
Eswar090906 <seswar2006@gmail.com>
2024-11-19 13:29:58 +0530

    added

---
+++ timetable/TT_App/migrations/Untitled-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    char namr[30]="Indis is our country GOOD";
+    char words[30]="One two Three\0four five";
+    printf("Name : %s\n",Name);
+    printf("Word : %s\n",World);
+    return(0);
+}
